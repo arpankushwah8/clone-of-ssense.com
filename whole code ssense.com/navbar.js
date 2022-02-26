2f8402951b326eaa608e032a105efe7a40256f3d
arpankushwah8 <96377007+arpankushwah8@users.noreply.github.com>
2022-02-26 07:28:48 +0530

    Add files via upload

---
+++ whole code ssense.com/navbar.js
@@ -0,0 +1,30 @@
+function navbar ()
+{
+    return`
+    <div id ="nav">
+    <ul id="navbar" id="main1">
+        <li><a href="menswear.html" id="mens1">MENSWEAR</a></li>
+        <li><a href="womenswear.html" id="womens1">WOMENWEAR</a></li>
+        <li><a href="#" id="mens1">EVERYTHING ELSE</a></li>
+        <li><a href="#" id="mens1">SEARCH</a></li>
+        <!-- <li style="font-size: 25px;
+        margin-left:8%; margin-right: 8%;margin-bottom: 10px;
+        
+        
+        
+        " ><a href="#">SSENSE </a></li> -->
+        <h1 class=" ssense" id="mens1">SSENSE</h1>
+       
+        <li><a href="#" id="mens1">ENGLISH</a></li>
+        <li><a href=""id="mens1">LOGIN</a></li>
+        <li><a href="#"id="mens1">WISHLIST</a></li>
+        <li><a href="#"id="mens1 " >SHOPPING BAG</a></li>
+        
+    </ul>
+</div>
+    
+    
+    
+    `
+}
+export default navbar 
